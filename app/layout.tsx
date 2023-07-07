f83f6a4eadbb0dba7cd2b66d90753e7667f311b5
Jason Pua <jasonpua@Jasons-MacBook-Air.local>
2023-07-07 22:07:50 +1000

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'Jason&Jolee Wedding',
-  description: 'Done by Jason',
+  title: 'Jason & Jolee Wedding',
+  description: 'RSVP Here - Done by Jason',
+  
 }
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
 import Hero from '@/components/Hero'
 import Image from 'next/image'
+import { Metadata } from 'next'
+ 
+
+export const metadata: Metadata = {
+  title: 'Jason & JoLee Wedding',
+  description: 'RSVP Here',
+}
+ 
 
 export default function Home() {
   return (
     <main className="">
-
+      <meta property="og:image" content="<generated>" />
+      <meta property="og:image:type" content="<generated>" />
+      <meta property="og:image:width" content="<generated>" />
+      <meta property="og:image:height" content="<generated>" />
       <Hero/>
     </main>
   )
