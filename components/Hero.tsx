7cdaf38dff68ffdad724c5895102ef57bbe99801
Jason Pua <jasonpua@Jasons-MacBook-Air.local>
2023-07-07 16:15:16 +1000

    removed typewriter

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,9 +2,9 @@
 import React from 'react'
 import { Cursor, Typewriter } from  'react-simple-typewriter';
 {/*import BackgroundCircles from "./BackgroundCircles"*/}
-import circlelogo from "@/public/assets/wedlogo91.png"
-import wedbg from "@/public/assets/wedbg.webp"
-import mobilebg from "@/public/assets/mobilebg.webp"
+import circlelogo from "@/public/assets/wedlogo92.svg"
+import wedbg from "@/public/assets/wedbg1.webp"
+import mobilebg from "@/public/assets/mobilebg1.webp"
 
 
 import Image from 'next/image'
@@ -29,11 +29,11 @@ export default function hero({}: Props) {
         {/*<BackgroundCircles />*/}
             <Image className="z-0 invisible md:visible" src= {wedbg} alt='logo' fill style={{objectFit:"cover"}}/>
             <Image className="z-0 md:invisible" src= {mobilebg} alt='logo' fill style={{objectFit:"cover"}}/>
-            <Image className="relative rounded-full mx-auto object-cover mb-8 z-10" src= {circlelogo} alt='logo'/>
+            <Image className="relative rounded-full mx-auto object-cover mb-8 z-10 fill-red-400" src= {circlelogo} alt='logo'/>
             {/* <img className=' relative rounded-full h-40 w-40 mx-auto object-cover mb-8' src="/assets/logo.jpg" alt="" /> */}
             <h1 className=' text-xl md:text-5xl lg:text-4xl text-[#F48377] font-serif p-10 z-10'>
                 <span>
-                  <Typewriter
+                  {/* <Typewriter
                     words={['You are invited to our Wedding!','See you on the 6th Jan 2024']}
                     // cursor
                     cursorStyle='_'
@@ -42,12 +42,12 @@ export default function hero({}: Props) {
                     deleteSpeed={50}
                     delaySpeed={1000}  
                   
-                  />
+                  /> */}
                 </span>
                 <Cursor cursorColor="#EF4444" cursorStyle='_'/>
             </h1>
             <a href="https://forms.gle/FyKxxWpVwj94kCpK7" className='z-10'>
-              <button className="bg-[#F48377] hover:bg-white text-white hover:text-[#F48377] font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out z-10">RSVP Here</button>
+              <button className="bg-[#E94D77] hover:bg-white text-white hover:text-[#E94D77] font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out z-10">RSVP Here</button>
             </a>
             {/* <h2 className=' text-2xl uppercase text-gray-500 pb-2 tracking-[15px]'>6th Jan 2024</h2> */}
 
